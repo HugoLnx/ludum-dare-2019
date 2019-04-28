af3895b97b5b18908d945f3944b7d0c53f4b227f
hugolnx <hugolnx@gmail.com>
2019-04-28 15:54:44 -0300

    HUD

---
+++ Assets/scripts/Child.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Child : MonoBehaviour
+{
+    public const string TAG = "child";
+}
--- Assets/scripts/witch/Witch.cs
+++ Assets/scripts/witch/Witch.cs
@@ -7,19 +7,40 @@ public class Witch : MonoBehaviour
 {
     public static string TAG = "witch";
     private CharMovement movement;
+    private bool carryingChild;
+    public int Score { get; private set; }
+
     void Awake()
     {
         this.movement = GetComponent<CharMovement>();
         this.movement.WalkSpeed = 2.5f;
     }
-    
-    void Update()
-    {
-        
-    }
 
     public void Dead()
     {
         Debug.Log("dead");
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        CheckCollision(collision);
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        CheckCollision(collision);    
+    }
+
+    private void CheckCollision(Collider2D collision)
+    {
+        if (collision.CompareTag(Child.TAG) && !carryingChild)
+        {
+            this.carryingChild = true;
+            GameObject.Destroy(collision.gameObject);
+        } else if (collision.CompareTag(Cauldron.TAG) && carryingChild)
+        {
+            this.carryingChild = false;
+            this.Score += 1000;
+        }
     }
 }

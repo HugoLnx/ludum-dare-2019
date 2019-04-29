60306c4e2e5f49dd7a63f3827b1b475b684d453f
hugolnx <hugolnx@gmail.com>
2019-04-29 17:31:41 -0300

    child new sprites

---
+++ Assets/scripts/AnimationControllerRandomizer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationControllerRandomizer : MonoBehaviour
+{
+    public RuntimeAnimatorController[] controllers;
+
+    void Awake()
+    {
+        var animator = GetComponent<Animator>();
+        animator.runtimeAnimatorController = controllers[Random.Range(0, controllers.Length)];
+    }
+}
